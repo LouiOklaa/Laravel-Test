034cb83ffc73317c636edb214a0884ca9bc4ebb0
LouiOklaa <louioklaa2001@gmail.com>
2022-05-20 02:34:01 -0800

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Api\ApiResponseTrait;
+use App\Http\Resources\ProductsResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+
+class ProductsController extends Controller
+{
+use ApiResponseTrait;
+
+    public function index(){
+
+        $products = ProductsResource::collection(Product::get());
+        return $this->ApiResponse($products,'Ok',200);
+
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'product_name' => 'required|unique:products|max:99',
+            'description' => 'required',
+            'image' => 'nullable|mimes:jpeg,png,jpg',
+        ]);
+
+        if ($validator->fails()) {
+
+            return $this->ApiResponse(null,$validator->errors(),400);
+
+        }
+
+        else{
+
+            $product = new Product();
+            $product->product_name = $request->product_name;
+            $product->description = $request->description;
+            if ($request->image && $request->image->isValid()){
+
+                //Get Random Name With Original Extension
+                $file_name = $request->file('image');
+                $image = rand() . '.' . $file_name->getClientOriginalExtension();
+
+                // Move Image
+                $request->image->move(public_path('Products_Image/'), $image);
+                $product->image = $image;
+
+            }
+            $product->save();
+
+            return $this->ApiResponse(new ProductsResource($product),'The Product Created!',201);
+
+        }
+
+    }
+
+    public function show($id){
+
+        $product = Product::find($id);
+
+        if($product){
+
+            return $this->ApiResponse(new ProductsResource($product),'Ok',200);
+        }
+        return $this->ApiResponse(null,'The Product Not Found!',404);
+
+    }
+
+    public function update(Request $request , $id){
+
+        $product = Product::find($id);
+
+        if(!$product){
+
+            return $this->ApiResponse(null,'The Product Not Found!',404);
+
+        }
+
+        $validator = Validator::make($request->all(), [
+            'product_name' => 'required|max:99|unique:products,product_name,'.$id,
+            'description' => 'required',
+            'image' => 'nullable|mimes:jpeg,png,jpg',
+        ]);
+
+        if ($validator->fails()) {
+
+            return $this->ApiResponse(null,$validator->errors(),400);
+
+        }
+
+        else{
+
+            $product->product_name = $request->product_name;
+            $product->description = $request->description;
+
+            if ($request->image && $request->image->isValid()){
+
+                //Delete Old Image
+                Storage::disk('public_uploads')->delete($product->image);
+
+                //Get Random Name With Original Extension
+                $file_name = $request->file('image');
+                $image = rand() . '.' . $file_name->getClientOriginalExtension();
+                $product->image = $image;
+
+                // Move Image
+                $request->image->move(public_path('Products_Image/'), $image);
+
+            }
+            $product->update();
+
+            return $this->ApiResponse(new ProductsResource($product),'The Product Updated!',201);
+
+        }
+
+    }
+
+    public function destroy($id){
+
+        $product = Product::find($id);
+
+        if(!$product){
+
+            return $this->ApiResponse(null,'The Product Not Found!',404);
+
+        }
+
+        $product->delete();
+        //Delete Image From Storage
+        Storage::disk('public_uploads')->delete($product->image);
+
+        return $this->ApiResponse(new ProductsResource($product),'The Product Deleted!',201);
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//All Auth Routes
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'Profile']);
+    Route::put('/me', [AuthController::class, 'Profile_Update']);
+});
+
+//All Products Routes
+Route::group([
+    'middleware' => ['jwt.verify']
+
+], function() {
+    Route::get('/products',[ProductsController::class,'index']);
+    Route::post('/products/create',[ProductsController::class,'store']);
+    Route::get('/products/{id}',[ProductsController::class,'show']);
+    Route::post('/products/update/{id}',[ProductsController::class,'update']);
+    Route::post('/products/destroy/{id}',[ProductsController::class,'destroy']);
+});
+
