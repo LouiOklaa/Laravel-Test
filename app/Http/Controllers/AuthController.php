62a29f7d6e1a96f0f4dcf690d88df69fc767f91a
LouiOklaa <louioklaa2001@gmail.com>
2022-05-21 11:16:54 -0800

    Create UserRepository

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Repository\UserRepositoryInterface;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
